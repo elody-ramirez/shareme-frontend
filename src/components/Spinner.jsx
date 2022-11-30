77aef21cf2d5621c6801bd727ba55c6d79a46671
elody-ramirez <elody.ramirez@gmail.com>
2022-11-30 04:08:48 -0500

    Created spinner component to display while loading feed

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Circles } from 'react-loader-spinner'
+
+const Spinner = ({ message }) => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <Circles 
+        color="#00BFFF"
+        height={50}
+        width={200}
+        className="m-5"
+        ariaLabel="loading"
+      />
+
+      <p className="text-lg text-center px-2">{message}</p>
+      <h2>test</h2>
+    </div>
+  )
+}
+
+export default Spinner
