ca37e39a11024a4472311fbac468aae485fdbbf2
elody-ramirez <elody.ramirez@gmail.com>
2022-12-03 20:56:19 -0500

    can show just the image for the pin followed by pin details

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -9,8 +9,46 @@ import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data'
 import Spinner from './Spinner'
 
 const PinDetail = ({ user }) => {
+  const [pins, setPins] = useState(null)
+  const [pinDetail, setPinDetail] = useState(null)
+  const [comment, setComment] = useState('')
+  const [addingComment, setAddingComment] = useState(false)
+  const { pinId } = useParams()
+
+  const fetchPinDetails = () => {
+    const query = pinDetailQuery(pinId)
+
+    if(query) {
+      client.fetch(query)
+        .then((data) => {
+          setPinDetail(data[0])
+
+          if (data[0]) {
+            query = pinDetailMorePinQuery(data[0])
+
+            client.fetch(query)
+              .then((res) => setPins(res))
+          }
+        })
+    }
+  }
+
+  useEffect(() => {
+    fetchPinDetails()
+  }, [pinId])
+
+  if(!pinDetail) return <Spinner message="Loading pin..."/>
+
   return (
-    <div>PinDetail</div>
+    <div className="flex xl-flex-row flex-col m-auto bg-white" style={{ maxWidth: '1500px', borderRadius: '32px' }}>
+      <div className="flex justify-center items-center md:items-start flex-initial">
+        <img
+          src={pinDetail?.image && urlFor(pinDetail.image).url()}
+          className="rounded-t-3xl rounded-b-lg"
+          alt="user-post" 
+        />
+      </div>
+    </div>
   )
 }
 
