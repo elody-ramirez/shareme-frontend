d842c3715c10afeb9d904a00b1b5314cf08d5395
elody-ramirez <elody.ramirez@gmail.com>
2022-12-03 20:51:47 -0500

    Added to queries to get the data of a pin

--- src/utils/data.js
+++ src/utils/data.js
@@ -53,6 +53,92 @@ export const categories = [
     }
 ];
 
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    image{
+        asset->{
+            url
+        }
+    },
+        _id,
+        destination,
+        postedBy->{
+            _id,
+            userName,
+            image
+        },
+        save[]{
+            _key,
+            postedBy->{
+                _id,
+                userName,
+                image
+            },
+        },
+}`
+
+export const pinDetailQuery = (pinId) => {
+	const query = `*[_type == "pin" && _id == '${pinId}']{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+   save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    }
+  }`
+	return query
+}
+
+export const pinDetailMorePinQuery = (pin) => {
+	const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`
+	return query
+}
+
 export const userQuery = (userId) => {
     const query = `*[_type == "user" && _id == '${userId}']`
 
@@ -85,26 +171,3 @@ export const searchQuery = (searchTerm) => {
 
     return query
 }
-
-export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
-    image{
-        asset->{
-            url
-        }
-    },
-        _id,
-        destination,
-        postedBy->{
-            _id,
-            userName,
-            image
-        },
-        save[]{
-            _key,
-            postedBy->{
-                _id,
-                userName,
-                image
-            },
-        },
-}`
