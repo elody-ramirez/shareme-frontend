7b594699b1c760c03fea34d27652f4353053d1b3
elody-ramirez <elody.ramirez@gmail.com>
2022-12-04 23:49:26 -0500

    Corrected a bug with the imports

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 
 import MasonryLayout from './MasonryLayout'
-import { client } from './client'
+import { client } from '../client'
 import { feedQuery, searchQuery } from '../utils/data'
 import Spinner from './Spinner'
 
